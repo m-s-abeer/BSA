ddd9dd31170bd21307f5d72d90dc4850aef9b5c8
Md. Hafizur Rahman Arfin <arfin97@gmail.com>
2019-06-02 06:28:33 +0630

    Profile and Homepage Update

--- Blue/urls.py
+++ Blue/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.HomePage.as_view(), name='home'),
@@ -29,3 +31,6 @@ urlpatterns = [
 
     path('announcement/', include("announcement.urls", namespace="announcement"), name="announcement"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
